f75c04ba475442612c43d9ff3ea4d785317acbca
AngeloPavone <pavone@gmx.us>
2022-12-18 22:47:22 -0800

    added parsing for instructions

--- Day5_SupplyStacks/solution.py
+++ Day5_SupplyStacks/solution.py
@@ -5,10 +5,18 @@ two     =       ['d','c','m']
 three   =       ['p']
 
 
-with open('small_input', 'r') as f:
-    lines = f.readlines()
+def parse_input():
+    with open('small_input', 'r') as f:
+        lines = f.readlines()
 
-    # parse input to remove all none numbers so that i can easily reference them
+# parse input to remove all none numbers and conver the numbers to int
+    instructions = []
     for instruction in lines:
-        instruction = re.split('move | from | to |\n', instruction) # can problem use re.search()
+        instruction = re.split('move | from | to |\n', instruction)
         instruction = instruction[1:-1]
+        instructions = [eval(i) for i in instruction]
+
+        print(instructions)
+
+if __name__ == '__main__':
+    parse_input()

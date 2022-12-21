86a9af0d454b088d0d3b2bc00169ac2545770f9b
AngeloPavone <pavone@gmx.us>
2022-12-21 15:45:54 -0800

    setup files

---
+++ Day6_TuningTrouble/solution.py
@@ -0,0 +1,15 @@
+
+
+with open('test_input', 'r') as f:
+    input = f.readlines()
+    # print(input)
+
+    for char in range(4, len(input), 1):
+        print(char)
+
+
+
+
+if __name__ == "__main__":
+
+    pass

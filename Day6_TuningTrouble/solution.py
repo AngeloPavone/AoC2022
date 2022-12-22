ebe1e646434b2f1d9ce2af75e77a71f5e72f6304
AngeloPavone <pavone@gmx.us>
2022-12-21 22:47:45 -0800

    added support for part 2 by just copy and pasting the function and changing the numbers

--- Day6_TuningTrouble/solution.py
+++ Day6_TuningTrouble/solution.py
@@ -21,6 +21,24 @@ def parse_input(input):
             pass
         buffer.clear()
 
+def parse_input2(input):
+    buffer = []
+    fuckme = set()
+
+    for char in range(len(input) - 13):
+        buffer.extend(input[char:char + 14])
+        try:
+            for char1 in buffer:
+                fuckme.add(char1)
+            if len(fuckme) == 14:
+                return char + 14
+            else:
+                fuckme.clear()
+        except:
+            pass
+        buffer.clear()
+
 
 if __name__ == "__main__":
     print(parse_input(get_input()))
+    print(parse_input2(get_input()))

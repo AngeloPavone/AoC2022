43aa35a2d12666949dc1e4298dcb4b1c6a877713
AngeloPavone <pavone@gmx.us>
2022-12-21 22:41:08 -0800

    im shooting myself

--- Day6_TuningTrouble/solution.py
+++ Day6_TuningTrouble/solution.py
@@ -1,13 +1,26 @@
-
 def get_input():
-    input = []
-    with open('test_input', 'r') as f:
-        for line in f:
-            input.append(line)
-    return input
+    with open('input', 'r') as f:
+        input = f.readlines()
+        result = input[0]
+    return result
 
 def parse_input(input):
-    pass
+    buffer = []
+    fuckme = set()
+
+    for char in range(len(input) - 3):
+        buffer.extend(input[char:char + 4])
+        try:
+            for char1 in buffer:
+                fuckme.add(char1)
+            if len(fuckme) == 4:
+                return char + 4
+            else:
+                fuckme.clear()
+        except:
+            pass
+        buffer.clear()
+
 
 if __name__ == "__main__":
-    parse_input(get_input())
+    print(parse_input(get_input()))
